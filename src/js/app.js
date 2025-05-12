09da971951a8c26b37a295e95209ccfcbc66cdcb
CarVill30 <carlosrogervillarreal3@gmail.com>
2025-05-12 16:01:10 +0000

    cambios

--- src/js/app.js
+++ src/js/app.js
@@ -37,10 +37,10 @@ function render(variables = {}) {
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          ${variables.twitter ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>` : ''}
+              ${variables.github ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>` : ''}
+              ${variables.linkedin ? `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>` : ''}
+              ${variables.instagram ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>` : ''}
           </ul>
         </div>
     `;
@@ -63,7 +63,7 @@ window.onload = function() {
     twitter: null,
     github: null,
     linkedin: null,
-    instagram: not null,
+    instagram: null,
     name: null,
     lastName: null,
     role: null,
