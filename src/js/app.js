aa52bce40cde8d8926bd037780fe5844124408b3
CarVill30 <carlosrogervillarreal3@gmail.com>
2025-05-15 04:18:38 +0000

    Creando las variables para los link

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,22 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name == null ? "Name" : variables.name} ${
-    variables.name
-  }</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.country} ${variables.city}</h3>
-          <ul class="position-right">
-          <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
+          <h1>${variables.name == null ? "name" : variables.name} 
+          ${variables.lastName == null ? "last name" : variables.lastName} 
+          </h1>
+          <h2>${variables.role == null ? "role" : variables.role}</h2>
+          <h3>${variables.country == null ? "country" : variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+          <li><a href="${
+            variables.twitter == null ? "Twitter" : variables.twitter
+          }
+          "><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github == null
+                ? "https://github.com/"
+                : "https://github.com/" + variables.github
+            }
+              "><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
           </ul>
